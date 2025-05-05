c208adddfee2b3f5d4991058e1c8ba98b4d7af60
agladkov <agladkov@logrocon.local>
2025-05-05 18:01:52 +0300

    добавлен инпут для диалога

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,23 @@
 const editor = document.querySelector('editor');
 const saveBtn = document.querySelector('actions__save-btn');
 const cancelBtn = document.querySelector('actions__cancel-btn');
+const uploadBtn = document.querySelector('actions__upoad-btn');
+const inputFile = document.querySelector('input-file');
+
+const invokeUpload = () => {
+    const inputFile = document.querySelector('input-file');
+    console.log("invokeUpload",inputFile)
+    inputFile && inputFile.click();
+}
+
+function loadFile(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        editor.value = e.target.result;
+    };
+    reader.readAsText(file);
+}
 
 // // Загрузка текста при запуске
 // window.api.loadText().then(text => {
